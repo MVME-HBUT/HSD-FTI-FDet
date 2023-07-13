bcef7aa78edb303dd80b42ecf5e0b97cd8fcbfe0
P-HL <87471007+P-HL@users.noreply.github.com>
2023-07-13 20:19:27 +0800

    update run shell

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,7 +6,7 @@ PORT=${PORT:-29200}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 
-CUDA_VISIBLE_DEVICES=2 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
 
 
